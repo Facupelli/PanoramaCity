384e10eb099b1a18410ed01ae5e8a6212c8b0a52
Facundo Pellicer <facundopellicer4@gmail.com>
2023-03-09 15:31:34 -0300

    GalleryButton

--- src/components/PropertyDetail/GalleryButtons.tsx
+++ src/components/PropertyDetail/GalleryButtons.tsx
@@ -1,16 +1,22 @@
+import Image from "~/icons/Image";
+import Tour from "~/icons/Tour";
+import Video from "~/icons/Video";
 import FavButton from "../UI/FavButton";
 
 export default function GalleryButtons() {
   return (
     <div className="flex items-center ">
-      <div className="flex max-w-xs grow items-center gap-4 font-semibold">
-        <button className="grow rounded-sm border border-neutral-200 bg-white p-2">
-          360°
+      <div className="flex max-w-sm grow items-center gap-4 font-semibold">
+        <button className="flex grow items-center justify-around rounded-sm border border-neutral-200 bg-white p-3">
+          <Tour />
+          Tour
         </button>
-        <button className="grow rounded-sm border border-neutral-200 bg-white p-2">
+        <button className="flex grow items-center justify-around rounded-sm border border-neutral-200 bg-white p-3">
+          <Image />
           Fotos
         </button>
-        <button className="grow rounded-sm border border-neutral-200 bg-white p-2">
+        <button className="flex grow items-center justify-around rounded-sm border border-neutral-200 bg-white p-3">
+          <Video />
           Video
         </button>
       </div>
